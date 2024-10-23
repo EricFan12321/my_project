b3a9b25b94b57c425731d3f27261bc42257fa525
Eric <liming.fan333@gmail.com>
2024-10-23 16:55:37 +0800

    update the directory structure tree

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
 #import the neccessary packages
 import torch
-import argparse
-import resnet # where ResNet() is defined
 import torchattacks
 import ssah_attack # where the SSAH is defined
 import yaml
 
-from auxiliary_utils import *
-
-# define a parse_arg(), so we could 
-# 1.swith between attack methods "SSAH", "PGD", "C&W" .etc
-# 2.change the parameters: 
-# -----batchsize, 
-#------dataset('cifar10', ''cifar100, 'imagenet'), 
-#------classifier('resnet20_cifar10', 'resnet20_cifar100', 'resnet50_imagenet')
-#------perceptual metric('ssim', 'lpips', 'l2' .etc)
-#------target mode('targeted' or 'untargeted'), if targeted , pick up a way to generate target labels
-
-
-
-
-# should I set the following parameters same for all attacks?
-# number_of_iterations
-# alpha
-# lambda
-# 
-
-# Set some parameters specific to each attack
-# if opt.perturb-mode === 'SSAH', we want to pop a window and ask:
-# (1) wavelet
-# (2) experiment_name
-
-# if opt.perturb-mode === 'SSAH', we want to pop a window and ask:
+from utils.utils import *
+from model.resnet import ResNet # where ResNet() is defined
 
 
 # Load configurations
@@ -57,20 +31,18 @@ elif config['dataset'] == 'cifar100':
 elif config['dataset'] == 'imagenet_val':
     data, num_images = load_imagenet_val(dataset_root, batch_size, num_workers)
 
-
-
 #Classifier: Load the classifier based on the dataset
 if config['dataset'] == 'cifar10':
     path = "checkpoints/cifar10-r20.pth.tar"
     checkpoint = torch.load(path)
     state = checkpoint['state_dict']
-    classifier = resnet.ResNet(20, 10)
+    classifier = ResNet(20, 10)
     classifier.load_state_dict(state)
 elif config['dataset'] == 'cifar100':
     path = "checkpoints/cifar100-r20.pth.tar"
     checkpoint = torch.load(path)
     state = checkpoint['state_dict']
-    classifier = resnet.ResNet(20, 100)
+    classifier = ResNet(20, 100)
     new_state_dict = OrderedDict()
     for k, v in state.items():
         if 'module.' in k:
@@ -93,7 +65,7 @@ for attack_config in config['attacks']:
                    num_iteration=attack_config['num_iteration'],
                    learning_rate=attack_config['learning_rate'],
                    device=device,
-                   Targeted=False,
+                   Targeted=True,
                    dataset=config['dataset'],
                    m=attack_config['m'],
                    alpha=attack_config['alpha'],
@@ -106,7 +78,10 @@ for attack_config in config['attacks']:
     elif attack_config['mode'] == 'JSMA':
         atk = torchattacks.JSMA(model, theta=attack_config['theta'], gamma=attack_config['gamma'])
        
-    
+atk.set_mode_targeted_by_label()
+
+#Get the target labels
+new_labels = (labels + 1) % 10
 
 
 # Evaluation
@@ -133,3 +108,30 @@ for batch, (inputs, targets) in enumerate(data): # enumerate(data) provides both
 # Compute the overall SAR for the entire test dataset
 sar = 100.0 * att_suc_img / total_img
 print(f"Overall Success Attack Rate (SAR): {sar:.2f}%")
+
+
+
+# define a parse_arg(), so we could 
+# 1.swith between attack methods "SSAH", "PGD", "C&W" .etc
+# 2.change the parameters: 
+# -----batchsize, 
+#------dataset('cifar10', ''cifar100, 'imagenet'), 
+#------classifier('resnet20_cifar10', 'resnet20_cifar100', 'resnet50_imagenet')
+#------perceptual metric('ssim', 'lpips', 'l2' .etc)
+#------target mode('targeted' or 'untargeted'), if targeted , pick up a way to generate target labels
+
+
+
+
+# should I set the following parameters same for all attacks?
+# number_of_iterations
+# alpha
+# lambda
+# 
+
+# Set some parameters specific to each attack
+# if opt.perturb-mode === 'SSAH', we want to pop a window and ask:
+# (1) wavelet
+# (2) experiment_name
+
+# if opt.perturb-mode === 'SSAH', we want to pop a window and ask:
